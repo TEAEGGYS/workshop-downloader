3d30cf802155a58ece613dec5f2acb82f6b703be
TEAEGGYS <x2425718339@sina.com>
2024-06-11 00:25:04 +0800

    立即执行函数

--- workshop-downloader.js
+++ workshop-downloader.js
@@ -1,7 +1,10 @@
-function parseQuery(url) {
-    let q = {};
-    url.replace(/([^?&=]+)=([^&]+)/g, (_, k, v) => (q[k] = v));
-    return q;
-}
-
-console.log(parseQuery('www.test.com/api/?id=100'));
+(function() {
+    'use strict';
+    function parseQuery(url) {
+        let q = {};
+        url.replace(/([^?&=]+)=([^&]+)/g, (_, k, v) => (q[k] = v));
+        return q;
+    }
+    
+    console.log(parseQuery('www.test.com/api/?id=100'));
+})();
